655c5bf3a941cf0e0302843d77d962e6ff5b8d11
sqlxpert <14205528+sqlxpert@users.noreply.github.com>
2025-10-11 19:26:28 -0700

    Draft Terraform module

---
+++ terraform/main.tf
@@ -0,0 +1,139 @@
+# AWS Step Function to stop RDS & Aurora databases after forced 7th-day start
+# github.com/sqlxpert/step-stay-stopped-aws-rds-aurora GPLv3 Copyright Marcelinƒ
+
+
+
+data "aws_iam_policy" "step_stay_stopped_do_role_local" {
+  count = var.step_stay_stopped_params["StepFnRoleAttachLocalPolicyName"] == "" ? 0 : 1
+
+  name = var.step_stay_stopped_params["StepFnRoleAttachLocalPolicyName"]
+}
+
+
+
+data "aws_kms_key" "step_stay_stopped_step_function" {
+  count = var.step_stay_stopped_params["StepFnKmsKey"] == "" ? 0 : 1
+
+  region = local.region
+  key_id = provider::aws::arn_build(
+    local.partition,
+    "kms", # service
+    local.region,
+    split(":", var.step_stay_stopped_params["StepFnKmsKey"])[0], # account
+    split(":", var.step_stay_stopped_params["StepFnKmsKey"])[1]  # resource (key/KEY_ID)
+  )
+}
+
+
+
+data "aws_kms_alias" "aws_sqs" {
+  count = var.step_stay_stopped_params["SqsKmsKey"] == "alias/aws/sqs" ? 1 : 0
+
+  region = local.region
+  name   = "alias/aws/sqs"
+}
+
+data "aws_kms_key" "step_stay_stopped_sqs" {
+  count = contains(["", "alias/aws/sqs"], var.step_stay_stopped_params["SqsKmsKey"]) ? 0 : 1
+
+  region = local.region
+  key_id = provider::aws::arn_build(
+    local.partition,
+    "kms", # service
+    local.region,
+    split(":", var.step_stay_stopped_params["SqsKmsKey"])[0], # account
+    split(":", var.step_stay_stopped_params["SqsKmsKey"])[1]  # resource (key/KEY_ID)
+  )
+  # Provider functions added in Terraform v1.8.0
+  # arn_build added in Terraform AWS provider v5.40.0
+}
+
+
+
+data "aws_kms_key" "step_stay_stopped_cloudwatch_logs" {
+  count = var.step_stay_stopped_params["CloudWatchLogsKmsKey"] == "" ? 0 : 1
+
+  region = local.region
+  key_id = provider::aws::arn_build(
+    local.partition,
+    "kms", # service
+    local.region,
+    split(":", var.step_stay_stopped_params["CloudWatchLogsKmsKey"])[0], # account
+    split(":", var.step_stay_stopped_params["CloudWatchLogsKmsKey"])[1]  # resource (key/KEY_ID)
+  )
+}
+
+
+
+locals {
+  step_stay_stopped_params = merge(
+    var.step_stay_stopped_params,
+    {
+      StepFnRoleAttachLocalPolicyName = try(
+        data.aws_iam_policy.step_stay_stopped_do_role_local[0].name,
+        ""
+      )
+
+      StepFnKmsKey = try(
+        join(":", [
+          provider::aws::arn_parse(data.aws_kms_key.step_stay_stopped_step_function[0].arn)["account_id"],
+          provider::aws::arn_parse(data.aws_kms_key.step_stay_stopped_step_function[0].arn)["resource"],
+        ]),
+        ""
+      )
+      SqsKmsKey = try(
+        data.aws_kms_alias.aws_sqs[0].name,
+        join(":", [
+          provider::aws::arn_parse(data.aws_kms_key.step_stay_stopped_sqs[0].arn)["account_id"],
+          provider::aws::arn_parse(data.aws_kms_key.step_stay_stopped_sqs[0].arn)["resource"],
+        ]),
+        ""
+      )
+      CloudWatchLogsKmsKey = try(
+        join(":", [
+          provider::aws::arn_parse(data.aws_kms_key.step_stay_stopped_cloudwatch_logs[0].arn)["account_id"],
+          provider::aws::arn_parse(data.aws_kms_key.step_stay_stopped_cloudwatch_logs[0].arn)["resource"],
+        ]),
+        ""
+      )
+    }
+  )
+}
+
+
+
+resource "aws_cloudformation_stack" "step_stay_stopped_prereq" {
+  name          = "StepStayStoppedRdsAuroraPrereq${var.step_stay_stopped_stack_name_suffix}"
+  template_body = file("${path.module}/../cloudformation/step_stay_stopped_aws_prereq.yaml")
+
+  region = local.region
+
+  capabilities = ["CAPABILITY_IAM"]
+  policy_body = file(
+    "${path.module}/../cloudformation/step_stay_stopped_aws_prereq_policy.json"
+  )
+
+  tags = local.step_stay_stopped_tags
+}
+
+data "aws_iam_role" "step_stay_stopped_deploy" {
+  name = aws_cloudformation_stack.step_stay_stopped_prereq.outputs["DeploymentRoleName"]
+}
+
+
+
+
+resource "aws_cloudformation_stack" "step_stay_stopped" {
+  name          = "StepStayStoppedRdsAurora${var.step_stay_stopped_stack_name_suffix}"
+  template_body = file("${path.module}/../cloudformation/step_stay_stopped_aws_rds_aurora.yaml")
+
+  region = local.region
+
+  capabilities = ["CAPABILITY_IAM"]
+  iam_role_arn = data.aws_iam_role.step_stay_stopped_deploy.arn
+  policy_body  = file("${path.module}/../cloudformation/step_stay_stopped_aws_policy.json")
+
+  parameters = local.step_stay_stopped_params
+
+  tags = local.step_stay_stopped_tags
+}
